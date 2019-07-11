4e6f3a52d1f871f1565bd3936f20833233d36e84
Jian <jxw7410@rit.edu>
2019-07-11 13:15:08 -0400

    fixed api name space issue

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < ApplicationController
+class Api::SessionsController < ApplicationController
 
     def create 
         user = User.find_by_credentials(session_params[:email], session_params[:password])
